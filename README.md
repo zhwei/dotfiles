ea8c495f5104947f9cc70abe474b2caf626da0c7
zhwei <sdutzhangwei@gamil.com>
2013-05-15 19:24:20 +0800

    delete some file && create auto_soft_link.sh

---
+++ README.md
@@ -0,0 +1,4 @@
+# zhwei's DOTFILES
+
+
++ 新增`SnipMate_for_django`
---
+++ auto_soft_link.sh
@@ -0,0 +1,11 @@
+#########################################################################
+# File Name: auto_soft_link.sh
+# Author: zhwei
+# mail: zhwei.yes@gmail.com
+# Created Time: Wed 15 May 2013 07:22:54 PM CST
+#########################################################################
+#!/bin/bash
+
+# SnipMate_for_django
+ln /home/zhwei/apps/snipmate_for_django/snippets/django.snippets /home/zhwei/config/VIM/.vim/bundle/snipmate.vim/snippets/
+ln /home/zhwei/apps/snipmate_for_django/snippets/htmldjango.snippets /home/zhwei/config/VIM/.vim/bundle/snipmate.vim/snippets/
