0a61b6652d73716bcf26999c32e546bfdc9e7860
zhwei <zhwei.yes@gmail.com>
2016-08-18 16:12:12 +0800

    方便运行php Server

--- scripts/laravel-share.sh
+++ scripts/laravel-share.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+sudo pwd
 tmux new-session -d 'sudo php artisan serv --host 127.0.0.1 --port 8080 -vvv'
 tmux split-window -v '~/dotfiles/bin/ngrok 127.0.0.1:8080'
 tmux split-window -h
